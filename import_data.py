bf8f241e9f28ae1eaae0c788c4aba2023bd0be8c
Emils Matiss <emils.matiss@gmail.com>
2021-07-12 11:16:08 -0400

    added basic imports and download function

---
+++ import_data.py
@@ -0,0 +1,31 @@
+import os
+import requests
+import numpy as np
+import seaborn as sns
+import pandas as pd
+
+
+def download():
+	fname = 'memory_nback.npz'
+	url = "https://osf.io/xfc7e/download"
+
+	if not os.path.isfile(fname):
+		print("Loading file...")
+		try:
+			r = requests.get(url)
+		except requests.ConnectionError:
+			print("!!! Failed to download data !!!")
+		else:
+			if r.status_code != requests.codes.ok:
+				print("!!! Failed to download data !!!")
+			else:
+				with open(fname, "wb") as fid:
+					fid.write(r.content)
+
+
+def main():
+	download()
+	exit(0)
+
+if __name__ == "__main__":
+	main()
