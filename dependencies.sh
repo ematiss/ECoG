18b792eeb6a0274facd513e6c684f4c22a24c3ed
Emils Matiss <emils.matiss@gmail.com>
2021-07-14 10:27:55 -0400

    added install script

---
+++ dependencies.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+pip install requests
+pip install numpy
+pip install seaborn
+pip install pandas
+pip install scipy
+pip install matplotlib
+pip install nilearn
+pip install nimare
+pip install duecredit
+pip install mne
